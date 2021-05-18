a2afdd2f88000cb35a63cdc4f5b625c092fbb776
DeepanshuMages <78214866+DeepanshuMages@users.noreply.github.com>
2021-05-18 15:20:16 +0530

    Grid System

---
+++ Assets/Script/GridSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSystem : MonoBehaviour
+{
+    public float size;
+    
+    public Vector3 GetNearestPointonGrid(Vector3 pos)
+    {
+        pos -= transform.position;
+
+        int xCount = Mathf.RoundToInt(pos.x/size);
+        int yCount = Mathf.RoundToInt(pos.y/size);
+        int zCount = Mathf.RoundToInt(pos.z/size);
+
+        Vector3 result = new Vector3(
+            (float)xCount * size, 
+            (float)yCount * size, 
+            (float)zCount * size);
+        result += transform.position;
+
+        return result;
+    }
+}
---
+++ Assets/Script/ObjectPlacer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPlacer : MonoBehaviour
+{
+    public GridSystem grid;
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hitInfo))
+            {
+                PlaceCubeNear(hitInfo.point);
+            }
+        }
+    }
+
+    private void PlaceCubeNear(Vector3 nearPoint)
+    {
+        var finalPosition = grid.GetNearestPointonGrid(nearPoint);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = finalPosition;
+
+    }
+}
