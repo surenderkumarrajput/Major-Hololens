167a3dfe7972e9083d2c320006dda68f11acd6ab
DeepanshuMages <78214866+DeepanshuMages@users.noreply.github.com>
2021-05-20 16:15:31 +0530

    Add : Read Json data from URL

---
+++ Assets/Script/ReadJson.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using LitJson;
+
+public class ReadJson : MonoBehaviour
+{
+    private JsonData JsonData;
+    private string jsonString;
+    public string url;
+
+    private void Start()
+    {
+        StartCoroutine(GetData());
+    }
+
+    private IEnumerator GetData()
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
+        {
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log("Error occured with web request");
+            }
+            else
+            {
+                jsonString = (webRequest.downloadHandler.text);
+                JsonData = JsonMapper.ToObject(jsonString);
+
+                print(JsonData["data"]["regional"][0]["loc"]);
+            }
+        }
+    }
+}
