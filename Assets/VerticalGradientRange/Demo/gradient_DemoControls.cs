38916924c4ab5912857acda1a8b7a3b2086e2ed6
DeepanshuMages <78214866+DeepanshuMages@users.noreply.github.com>
2021-06-13 16:41:50 +0530

    Added : Hologram effect, vertical gradient range for graph, India map state texture

---
+++ Assets/VerticalGradientRange/Demo/gradient_DemoControls.cs
@@ -0,0 +1,40 @@
+﻿// VerticalGradientRange by mgear / unitycoder.com
+// demo script for moving top/bottom lines up & down
+
+using UnityEngine;
+using System.Collections;
+
+public class gradient_DemoControls : MonoBehaviour {
+	
+	
+	public Transform topBar;
+	public Transform bottomBar;
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey("q"))
+		{
+			topBar.Translate(Vector3.up * Time.deltaTime);
+		}
+		
+		if (Input.GetKey("a"))
+		{
+			if (topBar.position.y>bottomBar.position.y)
+				topBar.Translate(-Vector3.up * Time.deltaTime);
+		}
+		
+		if (Input.GetKey("w"))
+		{
+			if (bottomBar.position.y<topBar.position.y)
+				bottomBar.Translate(Vector3.up * Time.deltaTime);
+		}
+		
+		if (Input.GetKey("s"))
+		{
+			bottomBar.Translate(-Vector3.up * Time.deltaTime);
+		}
+	
+	}
+}
